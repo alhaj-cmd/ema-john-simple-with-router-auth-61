30f3cbe94ab8175a7809b4b6750ae04996079722
github_username <you@domain.com>
2023-04-29 21:11:31 +0600

    signUp file add

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SingUp.css';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>Hello</h2>
+        </div>
+    );
+};
+
+export default SignUp;
